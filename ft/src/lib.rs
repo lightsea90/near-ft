e403f3ad16b1011d86985d368e4039ef3ec3dee9
TTNguyen <nttriet9a4@gmail.com>
2022-01-19 12:06:55 +0700

    tokens burn feature

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -35,6 +35,7 @@ pub struct Contract {
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 const TOKEN_FACTORY_ACCOUNT: &str = "token-factory.tokenhub.testnet";
+const BURN_TOKEN_ACCOUNT: &str = "burn-token.tokenhub.testnet";
 
 #[near_bindgen]
 impl Contract {
@@ -78,6 +79,25 @@ impl Contract {
         this.token.internal_register_account(owner_id.as_ref());
         this.token.internal_deposit(owner_id.as_ref(), total_supply.into());
         this
+    }
+
+    pub fn burn_token( 
+        &mut self,
+        account_id: ValidAccountId,
+        amount: Balance
+    ) {
+        assert!(
+            env::predecessor_account_id() == BURN_TOKEN_ACCOUNT,
+            "Only burn_token.token_factory.testnet account can execute the function"
+        );
+
+        self.token.internal_withdraw(account_id.as_ref(), amount);
+    }
+
+    pub fn total_supply(
+        &self
+    ) -> Balance {
+        self.token.total_supply
     }
 
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {

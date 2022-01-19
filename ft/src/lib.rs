70ed3128a3e9aaebd007d2c34d927c58cf2bc296
TTNguyen <nttriet9a4@gmail.com>
2022-01-19 17:26:24 +0700

    rm hardcode account

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -31,21 +31,28 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
+    token_factory_account: ValidAccountId,
+    burn_token_account: ValidAccountId
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
-const TOKEN_FACTORY_ACCOUNT: &str = "token-factory.tokenhub.testnet";
-const BURN_TOKEN_ACCOUNT: &str = "burn-token.tokenhub.testnet";
 
 #[near_bindgen]
 impl Contract {
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// default metadata (for example purposes only).
     #[init]
-    pub fn new_default_meta(owner_id: ValidAccountId, total_supply: U128) -> Self {
+    pub fn new_default_meta(
+        owner_id: ValidAccountId, 
+        total_supply: U128,
+        token_factory_account: ValidAccountId,
+        burn_token_account: ValidAccountId
+        ) -> Self {
         Self::new(
             owner_id,
             total_supply,
+            token_factory_account,
+            burn_token_account,
             FungibleTokenMetadata {
                 spec: FT_METADATA_SPEC.to_string(),
                 name: "Example NEAR fungible token".to_string(),
@@ -64,17 +71,21 @@ impl Contract {
     pub fn new(
         owner_id: ValidAccountId,
         total_supply: U128,
+        token_factory_account: ValidAccountId,
+        burn_token_account: ValidAccountId,
         metadata: FungibleTokenMetadata,
     ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         assert!(
-            env::predecessor_account_id() == TOKEN_FACTORY_ACCOUNT,
+            env::predecessor_account_id() == token_factory_account.to_string(),
             "Only token factory is allowed to execute the function"
         );
         metadata.assert_valid();
         let mut this = Self {
             token: FungibleToken::new(b"a".to_vec()),
             metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
+            token_factory_account,
+            burn_token_account
         };
         this.token.internal_register_account(owner_id.as_ref());
         this.token.internal_deposit(owner_id.as_ref(), total_supply.into());
@@ -87,7 +98,7 @@ impl Contract {
         amount: Balance
     ) {
         assert!(
-            env::predecessor_account_id() == BURN_TOKEN_ACCOUNT,
+            env::predecessor_account_id() == self.burn_token_account.to_string(),
             "Only burn_token.token_factory.testnet account can execute the function"
         );
 
